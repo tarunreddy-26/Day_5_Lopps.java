23557048d9d81d24a55ba578eb7b59bad3a2bed2
Tarun Reddy <2311cs010496@mallareddyuniversity.ac.in>
2025-01-07 13:10:03 +0530

    Create  Day_5_Loops.java

---
+++ Day_5_Loops.java
@@ -0,0 +1,12 @@
+public class Day_5_Loops {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 10; i++) {
+            System.out.println("shekar");
+        }
+        String s = "phone";
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(3));
+        }
+        System.out.println("b" + 1 + "d" + 2);
+    }
+}
